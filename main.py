27b7824159f5a32b7d3f40ffe2796bb8ba888c60
mikelash1 <mikelash1@gmail.com>
2018-02-09 00:40:28 -0800

    2-9-18

--- main.py
+++ main.py
@@ -9,147 +9,297 @@ import os
 import cv2
 import numpy as np
 import matplotlib as plt
+import random
+from collections import OrderedDict
 
-'''
-self.continents.append (Continent (['Congo', 'East Africa', 'Egypt', 'Madagascar', 'North Africa', 'South Africa']
-, 3, 'Africa', self))
-self.continents.append (Continent (['Alaska', 'Alberta', 'Central America', 'Eastern US', 'Greenland', 'Northwest Territories', 'Ontario', 'Quebec' , 'Western US']
-, 5, 'North America', self))
-self.continents.append (Continent ([ 'Venezuela', 'Brazil', 'Peru', 'Argentina']
-, 2, 'South America', self))
-self.continents.append (Continent ([Afghanistan ',' China ',' India ',' Irkutsk ',' Japan ',' Kamchatka ',' Middle East ',' Mongolia ',' Siam ',' Siberia ' , Ural ',' Yakutsk ']
-7, Asia 'self))
-self.continents.append (Continent (['Great Britain', 'Iceland', 'Northern Europe', 'Scandinavia', 'Southern Europe', 'Ukraine', 'Western Europe']
-5, 'Europe', self))
-self.continents.append (Continent (['Eastern Australia', 'Indonesia', 'New Guinea', 'Western Australia']
-, 2, 'Oceania', self))
+DEFAULT_BOARD_FILE = 'Risk_game_map.png'
 
-            self.continents[0].pays[0].voisins=[2,5,6]
-			self.continents[0].pays[1].voisins=[1,3,4,5,26]
-			self.continents[0].pays[2].voisins=[2,5,36,26]
-			self.continents[0].pays[3].voisins=[2,6]
-			self.continents[0].pays[4].voisins=[1,2,3,17,36,38]
-			self.continents[0].pays[5].voisins=[1,2,4]
-			self.continents[1].pays[0].voisins=[8,12,25]
-			self.continents[1].pays[1].voisins=[7,12,13,15]
-			self.continents[1].pays[2].voisins=[15,10,19]#Am centrale
-			self.continents[1].pays[3].voisins=[9,15,13,14]#10
-			self.continents[1].pays[4].voisins=[12,13,14,33]
-			self.continents[1].pays[5].voisins=[7,8,13,11]
-			self.continents[1].pays[6].voisins=[8,15,10,14,11,12]
-			self.continents[1].pays[7].voisins=[10,13,11]
-			self.continents[1].pays[8].voisins=[9,10,8,13]
-			self.continents[2].pays[0].voisins=[17,18]
-			self.continents[2].pays[1].voisins=[16,18,19,5]
-			self.continents[2].pays[2].voisins=[16,17,19]
-			self.continents[2].pays[3].voisins=[18,17,9]#Argentine
-			self.continents[3].pays[0].voisins=[21,22,26,30,37]#20
-			self.continents[3].pays[1].voisins=[20,22,28,27,29,30]
-			self.continents[3].pays[2].voisins=[20,21,26,28]
-			self.continents[3].pays[3].voisins=[29,27,25,31]
-			self.continents[3].pays[4].voisins=[27,25]
-			self.continents[3].pays[5].voisins=[31,23,27,24,7]
-			self.continents[3].pays[6].voisins=[20,22,37,2,3]
-			self.continents[3].pays[7].voisins=[24,21,29,25,23]
-			self.continents[3].pays[8].voisins=[21,22,40]
-			self.continents[3].pays[9].voisins=[30,21,23,31,27]
-			self.continents[3].pays[10].voisins=[20,21,29,37]#30
-			self.continents[3].pays[11].voisins=[29,23,25]
-			self.continents[4].pays[0].voisins=[33,35,34,38]
-			self.continents[4].pays[1].voisins=[32,35,11]
-			self.continents[4].pays[2].voisins=[32,35,37,36,38]
-			self.continents[4].pays[3].voisins=[37,32,33,34]
-			self.continents[4].pays[4].voisins=[38,34,37,3,26,5]
-			self.continents[4].pays[5].voisins=[35,34,36,20,26,30]
-			self.continents[4].pays[6].voisins=[32,34,36,5]
-			self.continents[5].pays[0].voisins=[42,41]
-			self.continents[5].pays[1].voisins=[42,41,28]#40
-			self.continents[5].pays[2].voisins=[42,40,39]
-			self.continents[5].pays[3].voisins=[39,41,40]
-'''
+global colors
+colors = {}
 
-colors = []
-for c in ['blue', 'green', 'red', 'yellow', 'orange', 'purple']:
-    norm = list(plt.colors.to_rgb('xkcd:dark {}'.format(c)))
-    light = list(plt.colors.to_rgb('xkcd:light {}'.format(c)))
-    
-    brgnorm = [int(i * 255) for i in reversed(norm)]
-    brglight = [int(i * 255) for i in reversed(light)]
-    colors.append([brgnorm, brglight])
-    #colors.append([brglight, brgnorm])
+global countries
+countries = {}
 
-def main():
+global players
+players = OrderedDict()
+
+global continents
+continents = {}
+
+def load_colors():
     
-    #img1 = cv2.imread('Pictures/Risk_game_map_fixed_greylevel.png')
-    img1 = cv2.imread('Risk_game_map.png')
-    #img1 = cv2.bitwise_not(img1)
-    #print(img1)
-    #return
+    global colors
+    for c in ['blue', 'green', 'red', 'yellow', 'orange', 'purple']:
+        norm = list(plt.colors.to_rgb('xkcd:dark {}'.format(c)))
+        light = list(plt.colors.to_rgb('xkcd:light {}'.format(c)))
+        
+        brgnorm = [int(i * 255) for i in reversed(norm)]
+        brglight = [int(i * 255) for i in reversed(light)]
+        colors[c] = [brgnorm, brglight]
     
-    cv2.imshow('image', img1)
-    cv2.waitKey(0)
+class Country:
+    def __init__(self, name, continent, borders):
+        self.name = name
+        self.continent = continent
+        self.borders = borders
+        self.owner = ''
+        self.troops = 0
+        
+        image_name = 'Pictures/{}.png'.format(name)
+        self.image = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
+        self.outline = cv2.Canny(self.image,100,200)
+        M = cv2.moments(self.image)
+        self.cX = int(M["m10"] / M["m00"])
+        self.cY = int(M["m01"] / M["m00"])
+        
+    def newOwner(self, owner):
+        global players
+        if self.owner and self.owner != owner:
+            print('{} captured {} from {}'.format(owner, self.name, self.owner))
+            players[self.owner].removeCountry(self.name)
+        
+        self.owner = owner
+        players[self.owner].addCountry(self.name)
+        self.troops = 0
+        
+    def addTroops(self, troops=1):
+        self.troops += troops
+        
+    def removeTroops(self, troops=1):
+        self.troops -= troops
+        
+class Player:
+    def __init__(self, color):
+        self.color = color
+        self.mycountries = []
+        
+    def addCountry(self, country_name):
+        self.mycountries.append(country_name)
+        
+    def removeCountry(self, country_name):
+        self.mycountries.remove(country_name)
+        
+    def takeTurn(self):
+        self.placeTroops(new_troops(set(self.mycountries)))
+        #show_board()
+        self.attack()
+        
+    def placeTroops(self, num_troops):
+        for it in range(num_troops):
+            random.shuffle(self.mycountries)
+            countries[self.mycountries[0]].addTroops()
+            
+    def attack(self):
+        global countries
+        random.shuffle(self.mycountries)
+        for c in [countries[c] for c in self.mycountries]:
+            if c.troops < 3:
+                continue
+            
+            for ec in c.borders:
+                if countries[ec].owner == self.color:
+                    continue
+                
+                while c.troops > 1 and countries[ec].troops > 0:
+                    attack_country(c, countries[ec])
+                    
+                if c.troops > 1:
+                    countries[ec].newOwner(self.color)
+                    self.move_troops(c, countries[ec], c.troops-1)
+                    
+                return
+            
+    def move_troops(self, from_country, to_country, num_move):
+        # Todo check for valid path and ownership
+        from_country.removeTroops(num_move)
+        to_country.addTroops(num_move)
+        
+def roll_die():
+    return random.randint(1,6)
+
+def rolls(num_rolls):
     
-    #for i in range(3):
-    i = -1
-    while True:
+    return sorted([roll_die() for i in range(num_rolls)], reverse=True)
+        
+def attack_country(attacking_country, defending_country, attacking_troops=3):
+    
+    attack_available = attacking_country.troops - 1
+    if attack_available < attacking_troops:
+        attacking_troops = attack_available
+        
+    defending_troops = 2 if defending_country.troops > 1 else 1
+    
+    attack_rolls = rolls(attacking_troops)
+    defend_rolls = rolls(defending_troops)
+    
+    i = 0
+    while i < min(attacking_troops, defending_troops):
+        if attack_rolls[i] > defend_rolls[i]:
+            defending_country.removeTroops()
+        else:
+            attacking_country.removeTroops()
+            
         i += 1
         
-        new_file = 'Pictures/Maps/{:02d}.png'.format(i+1)
-        if not os.path.isfile(new_file):
-            break
         
-        c = i % len(colors)
+def new_troops(my_countries):
+    
+    global continents
+    
+    # Country Count Troops
+    troops = int(len(my_countries) / 3)
+    if troops < 3:
+        troops = 3
         
-        #print(new_file)
-        image = cv2.imread(new_file, cv2.IMREAD_GRAYSCALE)
-        img1[np.where(image!=[0])] = colors[c][1]
+    # Continent Bonus
+    for n, cont in continents.items():
+        if cont <= my_countries:
+            troops += int(n[-1])
+            
+    return troops
+    
+def load_countries(country_file):
+    
+    global countries
+    global continents
+    
+    lines = []
+    with open(country_file, 'r') as fid:
+        lines = fid.readlines()
         
-        edges = cv2.Canny(image,100,200)
-        #cv2.imshow('image', edges)
-        #cv2.waitKey(0)   
+    for l in lines:
+        parts = l.strip().split()
+        countries[parts[0]] = Country(parts[0], parts[1], parts[2:])
         
-        img1[np.where(edges!=[0])] = colors[c][0]
+        continent = continents.get(parts[1], set())
+        continent.add(parts[0])
+        continents[parts[1]] = continent
+
+def initial_players(num_players=2):
+    
+    global players
+    global colors
+    global countries
+    
+    color_list = list(colors.keys())
+    
+    for i in range(num_players):
+        players[color_list[i]] = Player(color_list[i])
         
-        M = cv2.moments(image)
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
+    start_armies = 40 - 5 * (num_players - 2)
+    country_list = list(countries.keys())
+    random.shuffle(country_list)
+
+    for i in range(start_armies):
+        for pn, p in players.items():
+            if country_list:
+                c = country_list.pop()
+                countries[c].newOwner(pn)
+                countries[c].addTroops()
+            else:
+                random.shuffle(p.mycountries)
+                countries[p.mycountries[0]].addTroops()
+            
+            #show_board()
+                
+def show_country_background(base_image, country):
+
+    global colors
+    
+    if not country.owner:
+        return
         
-        # setup text
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        text = str(i+1)
-        border = 10
+    c = country.owner
+    
+    # Fill in the background
+    base_image[np.where(country.image!=[0])] = colors[c][1]
         
-        # get boundary of this text
-        textsize = cv2.getTextSize(text, font, 1, 2)[0]
+    # FIll in the edges
+    base_image[np.where(country.outline!=[0])] = colors[c][0]
+            
+def show_country_troops(base_image, country):
+    
+    global colors
+    
+    if not country.owner:
+        return
         
-        # get coords based on boundary
-        lowerL = int(cX - (textsize[0] + border) / 2)
-        lowerR = int(cY + (textsize[1] + border) / 2)
-        upperL = int(cX + (textsize[0] + border) / 2)
-        upperR = int(cY - (textsize[1] + border) / 2)
-        lowerLT = int(cX - textsize[0] / 2)
-        lowerRT = int(cY + textsize[1] / 2)
+    c = country.owner
+    
+    cX = country.cX
+    cY = country.cY
+    
+    # setup text
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text = str(country.troops)
+    border = 10
+    
+    # get boundary of this text
+    textsize = cv2.getTextSize(text, font, 1, 2)[0]
+    
+    # get coords based on boundary
+    lowerL = int(cX - (textsize[0] + border) / 2)
+    lowerR = int(cY + (textsize[1] + border) / 2)
+    upperL = int(cX + (textsize[0] + border) / 2)
+    upperR = int(cY - (textsize[1] + border) / 2)
+    lowerLT = int(cX - textsize[0] / 2)
+    lowerRT = int(cY + textsize[1] / 2)    
+    cv2.rectangle(base_image, (lowerL, lowerR), (upperL, upperR), colors[c][0], -1)
+    
+    # add text centered on image
+    cv2.putText(base_image, text, (lowerLT, lowerRT), font, 1, colors[c][1], 2)
+    
+def show_board(wait=0):
+    
+    global countries
+    base_image = cv2.imread(DEFAULT_BOARD_FILE)
+    
+    for cn, c in countries.items():
+        show_country_background(base_image, c)
         
-        cv2.rectangle(img1, (lowerL, lowerR), (upperL, upperR), colors[c][0], -1)
-        #cv2.putText(img1, str(i), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, \
-        #    colors[c][1], 2)
+    for cn, c in countries.items():
+        show_country_troops(base_image, c)
         
-        # add text centered on image
-        cv2.putText(img1, text, (lowerLT, lowerRT), font, 1, colors[c][1], 2)
+    cv2.imshow('image', base_image)
+    cv2.waitKey(wait)
         
-        # Detect blobs.
-        #keypoints = detector.detect(image)
- 
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        #img1 = cv2.drawKeypoints(img1, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
- 
-        #img1 += image
-        cv2.imshow('image', img1)
+def main():
+    
+    global players
+    
+    random.seed(1)
+    load_colors()
+    load_countries('Countries.txt')
+    initial_players(3)
+    
+    show_board()
+    
+    # Whoever picked last, goes first
+    player_list = list(players.keys())
+    player_list.reverse()
+    
+    while len(players) > 1:
         
-    cv2.waitKey(0)
+        ip = 0
+        while ip < len(players):
+            p = players[player_list[ip]]
+            if not p.mycountries:
+                del players[player_list[ip]]
+                continue
+            
+            p.takeTurn()
+            show_board()
+            ip += 1
+            
+        #break
+        
+    
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     
-    main()
+    try:
+        main()
+    except:
+        cv2.destroyAllWindows()
+        raise
--- temp.py
+++ temp.py
@@ -5,6 +5,18 @@ Created on Thu Feb  8 12:16:12 2018
 @author: LashMi
 """
 
+import shutil
+
+lines = []
+with open('Countries.txt', 'r') as fid:
+    lines = fid.readlines()
+
+
+for i,l in enumerate(lines):
+    old_pic = 'Pictures/Maps/{:02}.png'.format(i+1)
+    shutil.copyfile(old_pic, 'Pictures/{}.png'.format(l.split()[0]))
+
+'''
 lines = []
 with open('MapInfo.txt', 'r') as fid:
     lines = fid.readlines()
@@ -15,7 +27,7 @@ while lines[i].strip():
     useful = lines[i].split('[')[1].replace(']','').replace("'",'').split(',')[:-1]
     continent = useful[-1].strip() + useful[-2].strip()
     for u in useful[:-2]:
-        countries.append([u.strip().replace(' ','_'), continent])
+        countries.append([u.strip().replace(' ','_'), continent.replace(' ','_')])
         print(len(countries),u.strip())
     print(useful)
     i += 1
@@ -24,22 +36,26 @@ while lines[i].strip():
 
 #fid.write('\n')
 i += 1
+lines2 = []
 while lines[i].strip():
     
     nums = [int(n) for n in lines[i].strip().split()]
     i2 = nums.pop(0)
     nums.sort()
-    print(i2)
-    print(countries[i2 - 1][0])
+    #print(i2)
+    #print(countries[i2 - 1][0])
     #print(nums)
     str1 = '{}\t{}\n'.format(i2, ' '.join([str(n) for n in nums]))
     lines[i] = str1
-    print(str1.strip())
+    #print(str1.strip())
     #fid.write(str1)
     borders = [countries[n-1][0] for n in nums]
-    print(borders)
+    str2 = '{}\t{} {}'.format(countries[i2 - 1][0], countries[i2 - 1][1], ' '.join(borders))
+    print(str2)
+    lines2.append(str2)
     
     i += 1
 
-with open('MapInfo.txt', 'w') as fid:
-    fid.write(''.join(lines))
+with open('Countries.txt', 'w') as fid:
+    fid.write('\n'.join(lines2))
+    '''
